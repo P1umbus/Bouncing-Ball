e6a1e82577f156ee953ed198f44de807b08b8cca
Plumbus <1o1n1e1u1p1@gmail.com>
2021-12-01 15:27:46 +0200

    Start

---
+++ Assets/Ball/BallControl.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class BallControl : MonoBehaviour, IDragHandler
+{
+    [SerializeField] private Rigidbody _rb;
+    [SerializeField] private float _forceMagnitudeButtons;
+    [SerializeField] private float _forceMagnitudeTouches;
+
+    [SerializeField] private float _maxBallSpeed;
+
+    private float _testSpeed;
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            if (_rb.velocity.x > -5f)
+            {
+                _rb.velocity += -Vector3.right * Time.deltaTime * _forceMagnitudeButtons;
+                AddRandomTorque();
+            }
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            if (_rb.velocity.x < 5f)
+            {
+                _rb.velocity += Vector3.right * Time.deltaTime * _forceMagnitudeButtons;
+                AddRandomTorque();
+            }
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            if (_rb.velocity.y < 0f)
+            {
+                _rb.velocity += -Vector3.up * Time.deltaTime * _forceMagnitudeButtons * .1f;
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (_rb.velocity.magnitude > _maxBallSpeed)
+        {
+            _rb.velocity = Vector3.ClampMagnitude(_rb.velocity, _maxBallSpeed);
+        }
+    }
+
+    private void AddRandomTorque()
+    {
+        _rb.AddTorque(Random.Range(-10f, 10f), Random.Range(-10f, 10f), Random.Range(-10f, 10f));
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        float screenDifferenceMultiplier = 2160 / (float)Screen.height;
+
+        Vector3 vector = (Vector3)eventData.delta * Time.deltaTime * _forceMagnitudeTouches * screenDifferenceMultiplier;
+
+        _rb.velocity += vector;
+
+        if (_testSpeed < _rb.velocity.magnitude)
+        {
+            _testSpeed = _rb.velocity.magnitude;
+        }
+
+        AddRandomTorque();
+    }
+}
---
+++ Assets/Ball/FollowPosition.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPosition : MonoBehaviour 
+{    
+    [SerializeField] private Transform _target;
+    [SerializeField] private float _speed = 10f;
+    [SerializeField] private float _minY;
+    [SerializeField] private float _yIgnor;//забыл слово которым лучше назвать это поле(
+
+    void FixedUpdate() 
+    {
+        float x = Mathf.Lerp(transform.position.x, _target.position.x, Time.deltaTime * _speed);
+        float y = transform.position.y;
+
+        if (Mathf.Abs(transform.position.y - _target.position.y) > _yIgnor)
+            y = Mathf.Lerp(transform.position.y, _target.position.y, Time.deltaTime * _speed * 0.3f);
+
+        if (y < _minY)
+            y = _minY;
+
+        transform.position = new Vector3(x, y, 0);
+    }    
+}
