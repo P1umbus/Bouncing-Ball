067cae59907f57f611b03e1d06eccd5e280efc7c
P1umbus <1o1n1e1u1p1@gmail.com>
2021-12-28 17:38:07 +0200

    add ads

--- Assets/Scripts/Ball/BallControl.cs
+++ Assets/Scripts/Ball/BallControl.cs
@@ -6,11 +6,16 @@ public class BallControl : MonoBehaviour, IDragHandler
     [SerializeField] private Rigidbody _rb;
     [SerializeField] private Limiter _limiter;
     [Range(0.5f, 1.5f)]
-    [SerializeField] private float _forceMagnitudeTouches;
+    [SerializeField] private float _controlSensivity;
 
     [SerializeField] private float _limitVerticalBoostSpeed;
 
-    private float _verticalBoostSpeed; //!public
+    private float _verticalBoostSpeed;
+
+    private void Start()
+    {
+        _controlSensivity = PlayerPrefs.GetFloat(Constants.ControlSensivity);
+    }
 
     private void Update()
     {
@@ -25,9 +30,12 @@ public class BallControl : MonoBehaviour, IDragHandler
 
     public void OnDrag(PointerEventData eventData)
     {
+        if (_limiter.IsGround == true)
+            return;
+
         float screenDifferenceMultiplier = 2160 / (float)Screen.height; // 
 
-        Vector3 vector = (Vector3)eventData.delta * Time.deltaTime * _forceMagnitudeTouches * screenDifferenceMultiplier;
+        Vector3 vector = (Vector3)eventData.delta * Time.deltaTime * _controlSensivity * screenDifferenceMultiplier;
 
         if (_verticalBoostSpeed > _limitVerticalBoostSpeed)
             vector = new Vector3(vector.x, 0, 0);
--- Assets/Scripts/Shop/VisualBallManager.cs
+++ Assets/Scripts/Shop/VisualBallManager.cs
@@ -6,19 +6,23 @@ public class VisualBallManager : MonoBehaviour
 {
     [SerializeField] private Material[] _Materials;
     [SerializeField] private Renderer Ball;
+
     private void Awake()
     {
         GameEvent.SkinsUpdate += TryUpdateMaterial;
     }
+
     void Start()
     {
         TryUpdateMaterial();
     }
+
     private void TryUpdateMaterial()
     {
         var a = PlayerPrefs.GetInt("SelectedSkin");
         Ball.material = _Materials[a];
     }
+
     private void OnDestroy()
     {
         GameEvent.SkinsUpdate -= TryUpdateMaterial;
