a65f219b6bed853574cf133f1d0d35baa16d5e7b
VladVlados <karakayvsvs@gmail.com>
2021-12-30 16:03:30 +0200

    Clean, stable version

---
+++ Assets/Scripts/Vlad/SupScripts/Out/OutActiveLevel.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OutActiveLevel : MonoBehaviour
+{
+    private Text ActiveLevelText;
+    private void Awake()
+    {
+        ActiveLevelText = GetComponent<Text>();
+        //PlayerPrefs.SetInt(Constants.NumbActiveLevel, 1);
+    }
+    void Start()
+    {
+        OutActiveLevelInText();
+    }
+
+    private void OutActiveLevelInText()
+    {
+        var ActiveLevel = PlayerPrefs.GetInt(Constants.NumbActiveLevel);
+        if (Enum.IsDefined(typeof(Constants.GameLevelList), ActiveLevel))
+        {
+            ActiveLevelText.text = "Level " + (PlayerPrefs.GetInt(Constants.NumbActiveLevel));
+        }
+        else
+        {
+            ActiveLevelText.text = "Max Level";
+        }
+    }
+
+}
